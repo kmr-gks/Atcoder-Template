ca0e4d72baae21ec4e467a42832972b9fd23f22c
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-09-23 09:49:32 +0900

    自作クラスを使わないC++プロジェクト

---
+++ cppsample/cppsample.cpp
@@ -0,0 +1,11 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifdef _MSC_VER
+	//標準入力をファイルに設定
+	system("cd");
+	std::ifstream in("../include/atcoder-stdinput.txt");
+	std::cin.rdbuf(in.rdbuf());
+#endif
+}
